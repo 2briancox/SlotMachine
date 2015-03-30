7d6db10818ab9494e0439a2224056c2a938e8326
MacBook <2briancox@gmail.com>
2015-03-29 18:09:38 -0700

    Finished that practice code
    This was the finished code as presented in bitFountain.

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Brian on 3/24/15.
+//  Copyright (c) 2015 Melodity.com, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
